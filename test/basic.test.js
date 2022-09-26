a4c03fae8972c5ed597245c37f4037aa8e9794b0
Kymaeda <kyo.m.720@gmail.com>
2022-09-27 08:18:28 +0900

    learn vitest API(mather)
    - https://vitest.dev/api/#expect
    
    - `toBeCloseTo`
      - decimal calculation results are like below:
      ```
      expect(0.2 + 0.1).toBe(0.3) // 0.2 + 0.1 is 0.30000000000000004
      ```
      - in this case, you can use `toBeCloseTo`
      - https://vitest.dev/api/#tobecloseto
    
    - `toBeDefined`
      - https://vitest.dev/api/#tobedefined
      - check variables existance
      - null returns true, but undefined returns false

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,3 +3,22 @@ import { test, expect } from "vitest";
 test('2+2=4', () => {
   expect(2+2).toBe(4)
 })
+
+const iAmDefined = null;
+
+test('test api', () => {
+  // NOTE: in case of decimal calc, use `toBeCloseTo`
+  expect(0.2 + 0.1).toBeCloseTo(0.3);
+  // NOTE: `toBeDefined` returns false if variable is undefined
+  expect(iAmDefined).toBeDefined();
+  // NOTE: in js, false, null, undefined, '' and 0 is falsy value
+  expect("Hello").toBeTruthy()
+  expect(0).toBeFalsy()
+})
+
+class Example { }
+const example = new Example;
+test('object class is proper', () => {
+  expect(example).toBeDefined();
+  expect(example).toBeInstanceOf(Example);
+})

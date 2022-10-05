0cc7ee3cb2cd941604d07ca7440e3249703d132f
Kymaeda <kyo.m.720@gmail.com>
2022-10-06 08:46:10 +0900

    spy
    - https://vitest.dev/api/#vi-spyon

--- src/components/composables/useUser.spec.js
+++ src/components/composables/useUser.spec.js
@@ -43,3 +43,20 @@ test('mocking', () => {
   const mock = vi.fn().mockReturnValue(0.19)
   expect(calculator.tax(100, mock)).toBe(19);
 })
+
+test('spies', () => {
+  const spy = vi.spyOn(calculator, 'tax')
+  calculator.tax(100, realTaxRule)
+  expect(spy).toHaveBeenCalled()
+  expect(spy).toHaveReturnedWith(10)
+  expect(spy).toHaveBeenCalledWith(100, realTaxRule)
+
+  const spy2 = vi.fn(realTaxRule)
+  calculator.tax(100, spy2)
+  expect(spy2).toHaveBeenCalled()
+  expect(spy2).toHaveBeenCalledWith(100)
+
+  const spy3 = vi.fn(realTaxRule)
+  calculator.tax(0, spy3);
+  expect(spy3).not.toHaveBeenCalled();
+})
